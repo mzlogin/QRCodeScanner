ad953807fdbf96788a7c6e8b90e21c6e84135cb3
Zhuang Ma <chumpma@gmail.com>
2016-02-27 18:53:10 +0800

    Feature: grab window

---
+++ QRCodeScanner/QRCodeScanner.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-02-27T15:07:31
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = QRCodeScanner
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ QRCodeScanner/mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QScreen>
+#include <QLabel>
+#include <QApplication>
+#include <QDesktopWidget>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    // transparent background
+    setAttribute(Qt::WA_TranslucentBackground);
+
+    ui->setupUi(this);
+
+    ui->statusBar->hide();
+    ui->menuBar->hide();
+    ui->mainToolBar->hide();
+
+    connect(ui->startScan, SIGNAL(clicked()), this, SLOT(startScan()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::startScan()
+{
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QPoint pos = QWidget::mapToGlobal(this->rect().topLeft());
+    int x = pos.x();
+    int y = pos.y();
+    int w = this->width() / screen->devicePixelRatio();
+    int h = this->height() / screen->devicePixelRatio();
+
+    QPixmap pixmap = screen->grabWindow(QApplication::desktop()->winId(), x, y, w, h);
+
+    QLabel *ql = new QLabel();
+    ql->setPixmap(pixmap);
+    ql->show();
+}
